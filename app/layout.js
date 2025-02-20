973b3e736a30af22e5de64557ea08c689b3b3613
trungn <trungnguyen0803forwork@gmail.com>
2025-02-20 22:25:15 +0700

    modified metadata

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
       "I am a fullstack developer who loves creating efficient and scalable applications. My expertise lies in designing APIs, managing databases, and integrating systems. I am now enhancing my skills to transition into a DevOps Engineer role, where I am learning about cloud computing, automation, and managing infrastructure to deliver faster and more reliable software solutions. I believe in constantly improving my skills and aim to connect development and operations to achieve great results.",
     url: "https://trungn.site",
     siteName: "Trung Nguyen",
-    images: [
+    image: [
       {
         url: "https://trungn.site/logos/trungdeptraihihi.jpg",
         width: 1200,
