c8599527950ae0f48663a7ee17249d08320ecbdd
ttrungn <109566798+ttrungn@users.noreply.github.com>
2025-02-21 18:04:08 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,11 +11,11 @@ export const metadata = {
     title: "Trung Nguyen",
     description:
       "I am a fullstack developer who loves creating efficient and scalable applications. My expertise lies in designing APIs, managing databases, and integrating systems. I am now enhancing my skills to transition into a DevOps Engineer role, where I am learning about cloud computing, automation, and managing infrastructure to deliver faster and more reliable software solutions. I believe in constantly improving my skills and aim to connect development and operations to achieve great results.",
-    url: "https://trungn.site",
+    url: "https://trungn.works",
     siteName: "Trung Nguyen",
     images: [
       {
-        url: "https://trungn.site/logos/trungdeptraihihi.jpg",
+        url: "https://trungn.works/logos/trungdeptraihihi.jpg",
         alt: "trungdeptraihihi",
       },
     ],
