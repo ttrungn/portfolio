a081100fa75a31ba79f07022efcf6a4550e624f2
trungn <trungnguyen0803forwork@gmail.com>
2025-02-21 11:32:25 +0700

    modified tech stack

--- components/header.jsx
+++ components/header.jsx
@@ -4,7 +4,7 @@ import Socials from "./socials";
 
 const Header = (props) => {
   return (
-    <div className="lg:fixed h-screen basis-1.5/4 flex flex-col justify-between pb-48 self-center lg:self-auto">
+    <div className="lg:fixed h-screen basis-1.5/4 flex flex-col justify-between pb-48 self-center lg:self-auto text-center">
       <div className="">
         <h1 className="text-5xl subpixel-antialiased tracking-wide">
           {props.data.name}
--- components/techstacks/techstack.jsx
+++ components/techstacks/techstack.jsx
@@ -8,15 +8,21 @@ const TechStack = (props) => {
       <h2 className="text-xl mb-8 visible font-medium tracking-widest">
         Tech Stack
       </h2>
+      <div className="flex flex-wrap gap-4 mb-4">
+        <h6 className="visible font-medium tracking-widest mb-2">Primary</h6>
+        <div className="flex flex-wrap gap-4">
+          {props.data.primary.map(function (object, index) {
+            return <TechStackItem key={index} icon={object.icon} />;
+          })}
+        </div>
+      </div>
       <div className="flex flex-wrap gap-4">
-        {props.data.map(function (object, index) {
-          return (
-            <TechStackItem
-              key={`${object.position}+${object.startDate}`}
-              icon={object.icon}
-            />
-          );
-        })}
+        <h6 className="visible font-medium tracking-widest mb-2">Additional</h6>
+        <div className="flex flex-wrap gap-4">
+          {props.data.additional.map(function (object, index) {
+            return <TechStackItem key={index} icon={object.icon} />;
+          })}
+        </div>
       </div>
     </div>
   );
