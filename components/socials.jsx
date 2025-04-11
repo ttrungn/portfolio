f25ecaa14a1e15538711154b93fe9ecb399f614d
ttrungn <trungnguyen0803forwork@gmail.com>
2025-04-12 00:41:26 +0700

    update tech stacks

--- components/socials.jsx
+++ components/socials.jsx
@@ -37,9 +37,9 @@ const Socials = (props) => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             class="lucide lucide-file-user"
           >
             <path d="M14 2v4a2 2 0 0 0 2 2h4" />
